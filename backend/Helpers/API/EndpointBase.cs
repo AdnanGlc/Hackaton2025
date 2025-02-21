25d0eda07e079a97c9206e7aa8ce67b616b69118
AdnanGlc <adnan.galijasevic@edu.fit.ba>
2025-02-21 21:27:10 +0100

    Setup

---
+++ backend/Helpers/API/EndpointBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Helpers.API
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class EndpointBase : ControllerBase
+    {
+    }
+}
---
+++ backend/Helpers/API/EndpointBaseAsync.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Helpers.API
+{
+    public static class EndpointBaseAsync
+    {
+        public abstract class WithRequest<TRequest>
+        {
+            public abstract class WithResult<TResponse> : EndpointBase
+            {
+                public abstract Task<TResponse> HandleAsync(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                    );
+            }
+            public abstract class WithoutResult : EndpointBase
+            {
+                public abstract Task HandleAsync(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+            public abstract class WithActionResult<TResponse> : EndpointBase
+            {
+                public abstract Task<ActionResult<TResponse>> HandleAsync(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+            public abstract class WithActionResult : EndpointBase
+            {
+                public abstract Task<ActionResult> HandleAsync(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+        }
+        public static class WithoutRequest
+        {
+            public abstract class WithResult<TResponse> : EndpointBase
+            {
+                public abstract Task<TResponse> HandleAsync(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithoutResult : EndpointBase
+            {
+                public abstract Task HandleAsync(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithActionResult<TResponse> : EndpointBase
+            {
+                public abstract Task<ActionResult<TResponse>> HandleAsync(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithActionResult : EndpointBase
+            {
+                public abstract Task<ActionResult> HandleAsync(
+                    CancellationToken cancellationToken = default
+                );
+            }
+        }
+    }
+}
