95cefcb7066b5aa95c2d825d8153d3e7b9dbe0cb
AdnanGlc <adnan.galijasevic@edu.fit.ba>
2025-02-22 02:56:59 +0100

    backend setup

---
+++ backend/Models/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class Recipe
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Image { get; set; } = null!;
+
+    public int UserId { get; set; }
+
+    public virtual ICollection<RecipeProduct> RecipeProducts { get; set; } = new List<RecipeProduct>();
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ backend/Models/RecipeProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class RecipeProduct
+{
+    public int RecieptId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int QuantityKg { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual Recipe Reciept { get; set; } = null!;
+}
