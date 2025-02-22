e71cd79c92a08388d9381b475c35fd035d8282bf
Hasan Rahic <haso.raha10@gmail.com>
2025-02-22 06:39:30 +0100

    adding home and wizard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './pages/Login';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
+import Onboarding from './pages/Onboarding';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/onboarding" element={<Onboarding />} />
       </Routes>
     </Router>
   );
