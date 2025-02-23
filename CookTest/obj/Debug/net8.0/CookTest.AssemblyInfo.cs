d3c69f7689543998d5a1e1e1da25120cbcdccc4b
AdnanGlc <adnan.galijasevic@edu.fit.ba>
2025-02-23 06:29:13 +0100

    final final v2 :)

--- CookTest/obj/Debug/net8.0/CookTest.AssemblyInfo.cs
+++ CookTest/obj/Debug/net8.0/CookTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3595ec52493f59c1d00fcc85dd72320a7c6ec6fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bda13c4c5dd19a6d6515b1098b04c169b7917c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
